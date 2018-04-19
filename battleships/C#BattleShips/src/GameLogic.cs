04458463c2aa51c98b6359da1636c9f2ba6ec66c
Brock McCall <4952588@student.swin.edu.au>
2018-04-19 14:05:23 +1000

    Fixing maybe...

--- battleships/C#BattleShips/src/GameLogic.cs
+++ battleships/C#BattleShips/src/GameLogic.cs
@@ -1,23 +1,46 @@
-class GameLogic {
-    
-    public static void Main() {
-        // Opens a new Graphics Window
-        SwinGame.OpenGraphicsWindow("Battle Ships", 800, 600);
-        // Load Resources
-        LoadResources();
-        SwinGame.PlayMusic(GameMusic("Background"));
-        // Game Loop
-        for (
-        ; (((SwinGame.WindowCloseRequested() == true) 
-                    || (CurrentState == GameState.Quitting)) 
-                    == false); 
-        ) {
-            HandleUserInput();
-            DrawScreen();
+
+using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+//using System.Data;
+using System.Diagnostics;
+using SwinGameSDK;
+using System.Runtime.ExceptionServices;
+using System.Security;
+
+static class GameLogic
+{
+    [HandleProcessCorruptedStateExceptions]
+    [SecurityCritical]
+    public static int Main()
+	{
+		//Opens a new Graphics Window
+		SwinGame.OpenGraphicsWindow("Battle Ships", 800, 600);
+
+		//Load Resources
+		GameResources.LoadResources();
+
+		SwinGame.PlayMusic(GameResources.GameMusic("Background"));
+
+		//Game Loop
+		do {
+			GameController.HandleUserInput();
+			GameController.DrawScreen();
+		} while (!(SwinGame.WindowCloseRequested() == true | GameController.CurrentState == GameState.Quitting));
+
+		SwinGame.StopMusic();
+
+        //Free Resources and Close Audio, to end the program.
+        try {
+            GameResources.FreeResources();
+        } catch (Exception e)
+        {
+            System.Console.WriteLine("The following exception is due to .NET 4+");
+            System.Console.WriteLine(e.Message);
+            return 1;
         }
-        
-        SwinGame.StopMusic();
-        // Free Resources and Close Audio, to end the program.
-        FreeResources();
-    }
+
+        return 0;
+	}
 }
